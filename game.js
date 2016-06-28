14544cccace2aeb548ac7eb6cb44b489cb05cafb
Charlie Fish <fishcharlie@me.com>
2016-06-28 14:09:07 -0600

    Setting piece name for each tile

--- game.js
+++ game.js
@@ -47,7 +47,72 @@ board.prototype = {
 
 
 				//Set piece name
-				
+				if (index === 0) {
+					switch (i) {
+						case 0:
+							myTile.piecename="white-rook";
+							break;
+						case 1:
+							myTile.piecename="white-knight";
+							break;
+						case 2:
+							myTile.piecename="white-bishop";
+							break;
+						case 3:
+							myTile.piecename="white-queen";
+							break;
+						case 4:
+							myTile.piecename="white-king";
+							break;
+						case 5:
+							myTile.piecename="white-bishop";
+							break;
+						case 6:
+							myTile.piecename="white-knight";
+							break;
+						case 7:
+							myTile.piecename="white-rook";
+							break;
+						default:
+					}
+				}
+				if (index == 1) {
+					myTile.piecename="white-pawn";
+				}
+				if (index == 6) {
+					myTile.piecename="black-pawn";
+				}
+				if (index === 7) {
+					switch (i) {
+						case 0:
+							myTile.piecename="black-rook";
+							break;
+						case 1:
+							myTile.piecename="black-knight";
+							break;
+						case 2:
+							myTile.piecename="black-bishop";
+							break;
+						case 3:
+							myTile.piecename="black-queen";
+							break;
+						case 4:
+							myTile.piecename="black-king";
+							break;
+						case 5:
+							myTile.piecename="black-bishop";
+							break;
+						case 6:
+							myTile.piecename="black-knight";
+							break;
+						case 7:
+							myTile.piecename="black-rook";
+							break;
+						default:
+					}
+				}
+
+
 
 				self.gameArray[index][i]=myTile;
 			}
