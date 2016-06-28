ebda3fa0d0928ef73fcff129e7cbf2291d8bc078
Charlie Fish <fishcharlie@me.com>
2016-06-28 12:01:08 -0600

    Adding create tiles function and adding game array to board

--- game.js
+++ game.js
@@ -8,11 +8,22 @@ tile.prototype = {
 
 
 function board() {
+	this.gameArray = [[],[],[],[],[],[],[],[]];
 }
 
 board.prototype = {
 	addColors: function() {
 		console.log("Look at me!");
+	},
+	createTiles: function() {
+		var self = this;
+		this.gameArray.forEach(function(value, index, array) {
+			console.log(self);
+			for (var i=0; i<8; i++) {
+				var myTile = new tile();
+				self.gameArray[index][i]=myTile;
+			}
+		});
 	}
 };
 
