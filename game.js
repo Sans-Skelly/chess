dd01c5deacc054ca442207d9286ae9a8b1ba753a
Charlie Fish <fishcharlie@me.com>
2016-06-28 18:07:18 -0600

    Fixing formatting and switching all single quotes to double quotes

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 function tile() {
-	this.color = 'light';
-	this.htmlElement = '';
-	this.id = '';
-	this.piecename = '.';
-	this.piececolor = '.';
+	this.color = "light";
+	this.htmlElement = "";
+	this.id = "";
+	this.piecename = ".";
+	this.piececolor = ".";
 }
 
 tile.prototype = {
@@ -38,10 +38,10 @@ board.prototype = {
 
 				//Set Color
 				if ((i+index)%2===0) {
-					myTile.color = 'light-tile';
+					myTile.color = "light-tile";
 				}
 				else {
-					myTile.color = 'dark-tile';
+					myTile.color = "dark-tile";
 				}
 
 
@@ -119,77 +119,77 @@ board.prototype = {
 		});
 	},
 	setupClicks: function () {
-		$('#00').click(function() {clicked('00');});
-		$('#01').click(function() {clicked('01');});
-		$('#02').click(function() {clicked('02');});
-		$('#03').click(function() {clicked('03');});
-		$('#04').click(function() {clicked('04');});
-		$('#05').click(function() {clicked('05');});
-		$('#06').click(function() {clicked('06');});
-		$('#07').click(function() {clicked('07');});
+		$("#00").click(function() {clicked("00");});
+		$("#01").click(function() {clicked("01");});
+		$("#02").click(function() {clicked("02");});
+		$("#03").click(function() {clicked("03");});
+		$("#04").click(function() {clicked("04");});
+		$("#05").click(function() {clicked("05");});
+		$("#06").click(function() {clicked("06");});
+		$("#07").click(function() {clicked("07");});
 
-		$('#10').click(function() {clicked('10');});
-		$('#11').click(function() {clicked('11');});
-		$('#12').click(function() {clicked('12');});
-		$('#13').click(function() {clicked('13');});
-		$('#14').click(function() {clicked('14');});
-		$('#15').click(function() {clicked('15');});
-		$('#16').click(function() {clicked('16');});
-		$('#17').click(function() {clicked('17');});
+		$("#10").click(function() {clicked("10");});
+		$("#11").click(function() {clicked("11");});
+		$("#12").click(function() {clicked("12");});
+		$("#13").click(function() {clicked("13");});
+		$("#14").click(function() {clicked("14");});
+		$("#15").click(function() {clicked("15");});
+		$("#16").click(function() {clicked("16");});
+		$("#17").click(function() {clicked("17");});
 
-		$('#20').click(function() {clicked('20');});
-		$('#21').click(function() {clicked('21');});
-		$('#22').click(function() {clicked('22');});
-		$('#23').click(function() {clicked('23');});
-		$('#24').click(function() {clicked('24');});
-		$('#25').click(function() {clicked('25');});
-		$('#26').click(function() {clicked('26');});
-		$('#27').click(function() {clicked('27');});
+		$("#20").click(function() {clicked("20");});
+		$("#21").click(function() {clicked("21");});
+		$("#22").click(function() {clicked("22");});
+		$("#23").click(function() {clicked("23");});
+		$("#24").click(function() {clicked("24");});
+		$("#25").click(function() {clicked("25");});
+		$("#26").click(function() {clicked("26");});
+		$("#27").click(function() {clicked("27");});
 
-		$('#30').click(function() {clicked('30');});
-		$('#31').click(function() {clicked('31');});
-		$('#32').click(function() {clicked('32');});
-		$('#33').click(function() {clicked('33');});
-		$('#34').click(function() {clicked('34');});
-		$('#35').click(function() {clicked('35');});
-		$('#36').click(function() {clicked('36');});
-		$('#37').click(function() {clicked('37');});
+		$("#30").click(function() {clicked("30");});
+		$("#31").click(function() {clicked("31");});
+		$("#32").click(function() {clicked("32");});
+		$("#33").click(function() {clicked("33");});
+		$("#34").click(function() {clicked("34");});
+		$("#35").click(function() {clicked("35");});
+		$("#36").click(function() {clicked("36");});
+		$("#37").click(function() {clicked("37");});
 
-		$('#40').click(function() {clicked('40');});
-		$('#41').click(function() {clicked('41');});
-		$('#42').click(function() {clicked('42');});
-		$('#43').click(function() {clicked('43');});
-		$('#44').click(function() {clicked('44');});
-		$('#45').click(function() {clicked('45');});
-		$('#46').click(function() {clicked('46');});
-		$('#47').click(function() {clicked('47');});
+		$("#40").click(function() {clicked("40");});
+		$("#41").click(function() {clicked("41");});
+		$("#42").click(function() {clicked("42");});
+		$("#43").click(function() {clicked("43");});
+		$("#44").click(function() {clicked("44");});
+		$("#45").click(function() {clicked("45");});
+		$("#46").click(function() {clicked("46");});
+		$("#47").click(function() {clicked("47");});
 
-		$('#50').click(function() {clicked('50');});
-		$('#51').click(function() {clicked('51');});
-		$('#52').click(function() {clicked('52');});
-		$('#53').click(function() {clicked('53');});
-		$('#54').click(function() {clicked('54');});
-		$('#55').click(function() {clicked('55');});
-		$('#56').click(function() {clicked('56');});
-		$('#57').click(function() {clicked('57');});
+		$("#50").click(function() {clicked("50");});
+		$("#51").click(function() {clicked("51");});
+		$("#52").click(function() {clicked("52");});
+		$("#53").click(function() {clicked("53");});
+		$("#54").click(function() {clicked("54");});
+		$("#55").click(function() {clicked("55");});
+		$("#56").click(function() {clicked("56");});
+		$("#57").click(function() {clicked("57");});
 
-		$('#60').click(function() {clicked('60');});
-		$('#61').click(function() {clicked('61');});
-		$('#62').click(function() {clicked('62');});
-		$('#63').click(function() {clicked('63');});
-		$('#64').click(function() {clicked('64');});
-		$('#65').click(function() {clicked('65');});
-		$('#66').click(function() {clicked('66');});
-		$('#67').click(function() {clicked('67');});
+		$("#60").click(function() {clicked("60");});
+		$("#61").click(function() {clicked("61");});
+		$("#62").click(function() {clicked("62");});
+		$("#63").click(function() {clicked("63");});
+		$("#64").click(function() {clicked("64");});
+		$("#65").click(function() {clicked("65");});
+		$("#66").click(function() {clicked("66");});
+		$("#67").click(function() {clicked("67");});
 
-		$('#70').click(function() {clicked('70');});
-		$('#71').click(function() {clicked('71');});
-		$('#72').click(function() {clicked('72');});
-		$('#73').click(function() {clicked('73');});
-		$('#74').click(function() {clicked('74');});
-		$('#75').click(function() {clicked('75');});
-		$('#76').click(function() {clicked('76');});
-		$('#77').click(function() {clicked('77');});
+		$("#70").click(function() {clicked("70");});
+		$("#71").click(function() {clicked("71");});
+		$("#72").click(function() {clicked("72");});
+		$("#73").click(function() {clicked("73");});
+		$("#74").click(function() {clicked("74");});
+		$("#75").click(function() {clicked("75");});
+		$("#76").click(function() {clicked("76");});
+		$("#77").click(function() {clicked("77");});
 
 	}
 };
@@ -197,19 +197,19 @@ board.prototype = {
 
 
 function game() {
-	this.whosturn = 'black';
-	this.firstcell = '';
-	this.secondcell = '';
+	this.whosturn = "black";
+	this.firstcell = "";
+	this.secondcell = "";
 }
 
 game.prototype = {
 	switchTurns: function () {
-		if (this.whosturn=='black') {
-			this.whosturn='white';
+		if (this.whosturn=="black") {
+			this.whosturn="white";
 			$("#turntext").text("White turn");
 		}
-		else if (this.whosturn=='white') {
-			this.whosturn='black';
+		else if (this.whosturn=="white") {
+			this.whosturn="black";
 			$("#turntext").text("Black turn");
 		}
 
@@ -224,7 +224,7 @@ function clicked(id) {
 	var idString = id.toString();
 	var whosTurn = myGame.whosturn;
 
-	if (myGame.firstcell==='') {
+	if (myGame.firstcell==="") {
 		if (myBoard.gameArray[idString[0]][idString[1]].piececolor==whosTurn) {
 			myGame.firstcell=id;
 			$("#"+id).addClass("selected");
@@ -244,11 +244,11 @@ function clicked(id) {
 			alert("Can't move piece to same location");
 		}
 		else {
-		// if (validmove) {
-			myGame.secondcell=id;
-			movePiece();
-			//second move
-		// }
+			// if (validmove) {
+				//second move
+				myGame.secondcell=id;
+				movePiece();
+			// }
 		}
 	}
 }
@@ -262,12 +262,12 @@ function movePiece() {
 	cell2.piececolor=cell1.piececolor;
 
 	$("#"+myGame.firstcell).removeClass(cell1.piecename);
-	$("#"+myGame.firstcell).removeClass('selected');
-	cell1.piecename='.';
-	cell1.piececolor='.';
+	$("#"+myGame.firstcell).removeClass("selected");
+	cell1.piecename=".";
+	cell1.piececolor=".";
 
-	myGame.firstcell='';
-	myGame.secondcell='';
+	myGame.firstcell="";
+	myGame.secondcell="";
 
 	myGame.switchTurns();
 }
